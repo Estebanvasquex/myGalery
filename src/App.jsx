76efc98e78a73995efb0cb2dd8e50ba08f47f68b
Estebanvasquex <53843278+Estebanvasquex@users.noreply.github.com>
2023-07-08 22:10:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProfileContainer from './components/common/avatar/profileContainer'
+import ProfileContainer from './components/common/avatar/ProfileContainer'
 import ButtonSocialNetworksContainer from './components/common/buttonSocialNetworks/ButtonSocialNetworksContainer'
 
 import Galery from './components/pages/galery/Galery'
